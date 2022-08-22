cf8b115aa41a8650de79bce5f132557e9533d861
dadidange <david.gmelin@student.uni-luebeck.de>
2022-08-22 10:11:27 +0200

    update

--- naive.go
+++ naive.go
@@ -5,25 +5,25 @@ import "sort"
 
 type saSorter struct{
 	sa []int
-	idx []int
 	str []byte
+	size int
 }
 
 func (m saSorter) Len() int{
-	return len(m.idx) //is the same as len(m.Names)
+	return m.size
 }
 
 func (m saSorter) Less(i,j int) bool{
-	return less(m.sa[i], m.sa[j], m.str)
+	return less(m.sa[i], m.sa[j],m.size, m.str)
 }
 
-func less(i,j int, str[]byte) bool{
-	if i == len(str) || j == len(str) {
-		return i == len(str)
+func less(i,j,n int, str[]byte) bool{
+	if i == n || j == n {
+		return i == n
 	} else if str[i] != str[j]{
 		return str[i] < str[j]
 	} else { 
-		return less(i+1, j+1, str) 
+		return less(i+1, j+1, n, str) 
 	}
 }
 
@@ -33,17 +33,13 @@ func (m saSorter) Swap(i,j int){
 
 func saNaive(t []byte) []int {
 	n := len(t)
-	// if t[n-1] != '$'{
-	// 	n += 1
-	// 	t =append(t, '$')
-	// } 
 	indeces := make([]int, n)
 
 	for idx := range t{
 		indeces[idx] = idx
 	}
-	sa := saSorter{indeces, indeces, t}
+	sa := saSorter{indeces, t, n}
 	
 	sort.Sort(sa)
-	return sa.idx
+	return sa.sa
 }
